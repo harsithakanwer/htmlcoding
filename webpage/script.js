103a2c04b4150bd6822a63d358c3d5ada4493ec4
harsithakanwer <149561241+harsithakanwer@users.noreply.github.com>
2023-11-04 09:55:53 +0530

    Add files via upload

---
+++ webpage/script.js
@@ -0,0 +1,20 @@
+function overprop(){
+    const prop=document.getElementById('sub-menu1')
+    prop.style.display='block';
+    prop.style.position='absolute'
+}
+function overprop1(){
+    const prop=document.getElementById('sub-menu2')
+    prop.style.display='block';
+    prop.style.position='absolute'
+}
+function overprop2(){
+    const prop=document.getElementById('sub-menu3')
+    prop.style.display='block';
+    prop.style.position='absolute'
+}
+function overprop3(){
+    const prop=document.getElementById('sub-menu3')
+    prop.style.display='block';
+    prop.style.position='absolute'
+}
